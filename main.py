217a4b1a927cd5603f5cf0cbd816cbd829d894a8
saberamri <amrisaber.essai@gmail.com>
2021-09-25 01:31:12 +0200

    Added export  all category data in a single csv

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import wget
+import csv
 
 from books.bookinf import export_data
 from books.nextlinks import next_load
 from books.catload import href_load
 
 def main():
-    """trouver tous les url des livres à partir d'un lien de catégorie"""
+    """find all books urls from a category link"""
 
     url = "http://books.toscrape.com/catalogue/category/books_1/index.html"
     cat = href_load(url)
@@ -18,20 +19,26 @@ def main():
             li = export_data(lm)
             result.append(li)
             print(result)
+    
 
-    """export data
+    """export all category data in a single csv and all books images. 
+    """
+    clst = result[0].keys()
+    with open('export/All.csv', 'w', newline='') as data_output:
+        csv_output = csv.DictWriter(data_output, fieldnames = clst, delimiter=';')
+        csv_output.writeheader()
+        for row in result:
+            image_filename = wget.download(row["image_url"], out = "export/images")
+            print('Image Successfully Downloaded: ', image_filename)
+        csv_output.writerows(result)
+
+
+    """export all category data each category in a csv file
     """
     df_from_dict = pd.DataFrame.from_dict(result)
     df_grouped = df_from_dict.groupby("category")
     for k, gr in df_grouped:
         gr.to_csv('export/{}.csv'.format(k), sep=';', index=False, encoding='utf-8-sig')
-
-    """
-    export images
-    """
-    for elem in result:
-        image_filename = wget.download(elem["image_url"], out = "export/images")
-        print('Image Successfully Downloaded', image_filename)
 
 
 if __name__ == "__main__":
